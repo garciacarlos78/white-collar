38814557c807312cb30c13e5c05d2bf005c77a2f
garciacarlos78 <51118101+garciacarlos78@users.noreply.github.com>
2020-03-10 09:41:28 +0100

    Initial commit

---
+++ src/test/java/com/cgrdev/whitecollar/WhiteCollarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgrdev.whitecollar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteCollarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
